a16c220049f2a90189c9d03495bc4f40699bd0ca
Davey <daveymoores@gmail.com>
2016-01-08 11:44:45 +0000

    fixing buttons on home page

--- js/homescript.js
+++ js/homescript.js
@@ -444,7 +444,7 @@ $(document).ready(function() {
           });
 
           $('#jsAppIconsFirst').velocity("stop", true).velocity({
-             marginTop: '30vh',
+             marginTop: '0px',
              opacity: '0'
           }, 450,  easingQuadOut);
 
@@ -481,7 +481,7 @@ $(document).ready(function() {
 
 
           $('#jsAppIconsFirst').velocity("stop", true).velocity({
-             marginTop: '30vh',
+             marginTop: '0px',
              opacity: '1'
           }, 450,  easingQuadOut);
       }
